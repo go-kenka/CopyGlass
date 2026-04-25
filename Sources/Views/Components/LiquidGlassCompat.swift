75192ddfa13a4c945b4b7fecff5a27af5623a2eb
wujianhua <1107015496@qq.com>
2026-04-25 10:24:29 +0800

    feat(Views): 添加COPYGLASS_ENABLE_LIQUID_GLASS条件编译支持
    为LiquidGlassCompat组件添加条件编译支持，通过COPYGLASS_ENABLE_LIQUID_GLASS宏控制是否启用液体玻璃效果

--- Sources/Views/Components/LiquidGlassCompat.swift
+++ Sources/Views/Components/LiquidGlassCompat.swift
@@ -9,15 +9,20 @@ enum LiquidGlassStyle {
 extension View {
     @ViewBuilder
     func liquidGlassIfAvailable() -> some View {
+        #if COPYGLASS_ENABLE_LIQUID_GLASS
         if #available(macOS 26.0, *) {
             glassEffect()
         } else {
             self
         }
+        #else
+        self
+        #endif
     }
     
     @ViewBuilder
     func liquidGlassIfAvailable<S: Shape>(_ style: LiquidGlassStyle, in shape: S) -> some View {
+        #if COPYGLASS_ENABLE_LIQUID_GLASS
         if #available(macOS 26.0, *) {
             switch style {
             case .regular:
@@ -30,15 +35,22 @@ extension View {
         } else {
             self
         }
+        #else
+        self
+        #endif
     }
     
     @ViewBuilder
     func backgroundExtensionIfAvailable() -> some View {
+        #if COPYGLASS_ENABLE_LIQUID_GLASS
         if #available(macOS 26.0, *) {
             backgroundExtensionEffect()
         } else {
             self
         }
+        #else
+        self
+        #endif
     }
 }
 
@@ -53,6 +65,7 @@ struct LiquidGlassChip: View {
             .padding(.vertical, 6)
             .liquidGlassIfAvailable(isProminent ? .regular : .clear, in: Capsule(style: .continuous))
             .background {
+                #if COPYGLASS_ENABLE_LIQUID_GLASS
                 if #available(macOS 26.0, *) {
                     Color.clear
                 } else {
@@ -63,6 +76,14 @@ struct LiquidGlassChip: View {
                                 .stroke(Color.white.opacity(0.15), lineWidth: 1)
                         }
                 }
+                #else
+                Capsule(style: .continuous)
+                    .fill(.thinMaterial)
+                    .overlay {
+                        Capsule(style: .continuous)
+                            .stroke(Color.white.opacity(0.15), lineWidth: 1)
+                    }
+                #endif
             }
     }
 }
